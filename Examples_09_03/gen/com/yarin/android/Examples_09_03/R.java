0128583f9edfca808c4cbe663602f523c5cb8972
panchenji <panchenji@gmail.com>
2011-03-30 18:06:48 +0800

    finish the two main functions

--- Examples_09_03/gen/com/yarin/android/Examples_09_03/R.java
+++ Examples_09_03/gen/com/yarin/android/Examples_09_03/R.java
@@ -14,13 +14,16 @@ public final class R {
         public static final int arrow=0x7f020000;
         public static final int home=0x7f020001;
         public static final int icon=0x7f020002;
+        public static final int push_pin=0x7f020003;
     }
     public static final class id {
-        public static final int MapView01=0x7f050001;
-        public static final int TextView01=0x7f050000;
+        public static final int MapView01=0x7f050002;
+        public static final int TextView01=0x7f050001;
+        public static final int username=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dialog=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Examples_09_03/src/com/yarin/android/Examples_09_03/Activity01.java
+++ Examples_09_03/src/com/yarin/android/Examples_09_03/Activity01.java
@@ -2,41 +2,79 @@ package com.yarin.android.Examples_09_03;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+
+
+import android.widget.EditText;
 //import com.yarin.android.Examples_09_04.R;
 
 public class Activity01 extends MapActivity
 {
 	private MapView	 mMapView;
 	private MapController mMapController; 
-	private GeoPoint mGeoPoint;
+	private GeoPoint mGeoPoint,mGeoPoint_search;
 	private int num_site=0;
 	List<PinOverlay> PinOverlays=new ArrayList<PinOverlay>();
 	private SharedPreferences sites;
+	private boolean pin=false;
+    private static final int Pin_Start=Menu.FIRST; 
+    private static final int Pin_End=Menu.FIRST+1;
+    private static final int My_Location=Menu.FIRST+2;
+    
+    Location location;
+    LocationManager locationManager;
+    Criteria criteria =new Criteria();
+    String provider="";
+    
+	//Geocoder gc=new Geocoder(this,Locale.CHINA);
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.main);
+		
+        
+        String context=Context.LOCATION_SERVICE;
+        locationManager=(LocationManager)getSystemService(context);
+        
 		mMapView = (MapView) findViewById(R.id.MapView01);
 		//设置为交通模式
 		//mMapView.setTraffic(true);
@@ -57,17 +95,150 @@ public class Activity01 extends MapActivity
 		//设置倍数(1-21)
 		mMapController.setZoom(12); 
 		sites=getSharedPreferences("sites",0);
+		
+		
+		//设置Criteria（服务商）的信息
+        
+        //经度要求
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(false);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        //取得效果最好的criteria
+        provider=locationManager.getBestProvider(criteria, true);
+        //得到坐标相关的信息
+        location=locationManager.getLastKnownLocation(provider);
+        //更新坐标
+       // updateWithNewLocation(location);
+        //注册一个周期性的更新，3000ms更新一次
+		//locationListener用来监听定位信息的改变
+        //locationManager.requestLocationUpdates(provider, 3000, 0,locationListener);
 		//添加Overlay，用于显示标注信息
         MyLocationOverlay myLocationOverlay = new MyLocationOverlay();
         List<Overlay> list = mMapView.getOverlays();
         list.add(myLocationOverlay);
+        
 	}
+    private void UpdateMyLocation() 
+    {
+    	 location=locationManager.getLastKnownLocation(provider);
+    	 mGeoPoint = new GeoPoint((int) (location.getLatitude() * 1000000), (int) (location.getLongitude() * 1000000));
+    	 mMapController.animateTo(mGeoPoint); 
+    }
 	protected boolean isRouteDisplayed()
 	{
 		return false;
 	}
 	
-	
+	public boolean onKeyDown(int KeyCode,KeyEvent event)
+	{
+		switch(KeyCode)
+		{
+		case KeyEvent.KEYCODE_SEARCH:
+			final LayoutInflater factory = LayoutInflater.from(Activity01.this);
+			//得到自定义对话框
+		    final View DialogView = factory.inflate(R.layout.dialog, null);
+            //创建对话框
+            AlertDialog dlg = new AlertDialog.Builder(Activity01.this)
+            .setTitle("搜索")
+            .setView(DialogView)//设置自定义对话框的样式
+            .setPositiveButton("确定", //设置"确定"按钮
+            new DialogInterface.OnClickListener() //设置事件监听
+            {
+                public void onClick(DialogInterface dialog, int whichButton) 
+                {
+                	//输入完成后，点击“确定”开始登陆
+                	//m_Dialog = ProgressDialog.show
+                     //          (
+                      //      	 Activity01.this,
+                       //          "请等待...",
+                        //         "正在为你登录...", 
+                         //        true
+                    
+                	//     );
+                	//System.out.println('k');
+                	String search_content="1600 Pennsylvania Ave, Washington DC";
+                    //search_content=((EditText)DialogView.findViewById(R.id.username)).getText().toString();
+                	change_center(search_content);
+                	
+                    
+                    
+                }
+            })
+            .setNegativeButton("取消", //设置“取消”按钮
+            new DialogInterface.OnClickListener() 
+            {
+                public void onClick(DialogInterface dialog, int whichButton)
+                {
+                	dialog.cancel();
+                }
+            })
+            .create();//创建
+            dlg.show();//显示
+		}
+
+           return super.onKeyDown(KeyCode, event);
+		
+		}
+	public void change_center(String search_content)
+	{
+		Geocoder gc= new Geocoder(this, Locale.getDefault());
+    	
+        System.out.println(search_content+"tmdnmb");
+      //  double lat=0.0;
+       // double lng=0.0;
+        try{
+        	List<Address> addresses = gc.getFromLocationName(search_content, 5);
+        	//String add = "";
+        	
+        	if (addresses.size() > 0) {
+        		mGeoPoint_search = new GeoPoint((int) (addresses.get(0).getLatitude() * 1000000),
+        				(int) (( addresses.get(0)).getLongitude() * 1000000));
+        		//定位到成都
+        		mMapController.animateTo(mGeoPoint_search); 
+        		mMapView.invalidate();
+        	}
+        	else{
+        		DisplayToast("Cannot find the address");
+        		
+        	}
+    	
+        }catch(Exception e){DisplayToast("error:"+e);};
+		
+	}
+    public boolean onCreateOptionsMenu(Menu menu)
+	{
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, Pin_Start, Menu.NONE, "标记");
+		menu.add(0,Pin_End,Menu.NONE,"关闭标记");
+		menu.add(0, My_Location, Menu.NONE, "我的位置");
+		
+		
+		return true;
+	}
+    
+    public boolean onOptionsItemSelected(MenuItem item)
+	{
+		super.onOptionsItemSelected(item);
+		switch (item.getItemId())
+		{
+			case (Pin_Start):
+				//开启标记
+				pin=true;
+				return true;
+
+			case (Pin_End):
+				//取消标记
+			    pin=false;
+			
+				return true;
+			case (My_Location):
+				//我的位置
+				UpdateMyLocation();
+		}
+		return true;
+	}
 	public void DisplayToast(String str)
 	{
 		Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
@@ -108,11 +279,13 @@ public class Activity01 extends MapActivity
 			paint.setStrokeWidth(1);
 			paint.setARGB(255, 255, 0, 0);
 			paint.setStyle(Paint.Style.STROKE);
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.arrow);
+			paint.setColorFilter(new LightingColorFilter(Color.WHITE, 0x80000000));
+			Bitmap bmparrow = BitmapFactory.decodeResource(getResources(), R.drawable.push_pin);
+			//paint.setAlpha(00);
 			Point PinCoords=new Point(0,0);
 			mMapView.getProjection().toPixels(GeoPinCoords,PinCoords);
 			
-			canvas.drawBitmap(bmp, PinCoords.x, PinCoords.y, paint);
+			canvas.drawBitmap(bmparrow, PinCoords.x-bmparrow.getWidth(), PinCoords.y-bmparrow.getHeight(), paint);
 			//canvas.drawText("天府广场", myScreenCoords.x, myScreenCoords.y, paint);
 		//	return true;
 		}
@@ -124,6 +297,7 @@ public class Activity01 extends MapActivity
 		public boolean onTouchEvent(MotionEvent event,MapView mv)
 		{
 			//TextView Touch_LocationText = (TextView)findViewById(R.id.TextView01);
+			super.onTouchEvent(event, mv);
 			int iAction=event.getAction();
 			if(iAction==MotionEvent.ACTION_CANCEL||
 				iAction==MotionEvent.ACTION_DOWN||
@@ -131,6 +305,7 @@ public class Activity01 extends MapActivity
 			{
 				return false;
 			}
+			if(pin==true){
 			//DisplayToast("aaaaaaa");
 			int x=(int)event.getX();
 			int y=(int)event.getY();
@@ -141,6 +316,7 @@ public class Activity01 extends MapActivity
 			GeoPoint Touch_GeoPoint;
 			//Location Touch_Location;
 			Touch_GeoPoint=mMapView.getProjection().fromPixels(x, y);
+			
 			double Touch_latitude = ((double)Touch_GeoPoint.getLatitudeE6())/1000000;
 			double Touch_longitude = ((double)Touch_GeoPoint.getLongitudeE6())/1000000;
 		//	Touch_Location.setLatitude(Touch_latitude);
@@ -148,15 +324,27 @@ public class Activity01 extends MapActivity
 			//Touch_LocationText.setText("you choose xxxxxxx");
 			SharedPreferences sites=getSharedPreferences("site",0);
 			SharedPreferences.Editor editor=sites.edit();
+            double lat_abs=0.000000;
+            double long_abs=0.000000;
 			int i;
 			for(i=0;i<num_site;i++)
 			{
-				if(Double.toString(Touch_latitude).equals(sites.getString("sitelat" +
-						Integer.toString(i), ""))&&
-						Double.toString(Touch_longitude).equals(sites.getString("sitelong" +
-								Integer.toString(i), ""))) break;
+				lat_abs=Touch_latitude-Double.parseDouble(sites.getString("sitelat" +
+						Integer.toString(i), ""));
+				long_abs=Touch_longitude-Double.parseDouble(sites.getString("sitelong" +
+						Integer.toString(i), ""));
+				
+				if((Math.abs(lat_abs)<=1E-2)&&(Math.abs(long_abs)<=1E-2)) 
+				{
+					Touch_latitude=Double.parseDouble(sites.getString("sitelat" +
+							Integer.toString(i), ""));
+					Touch_longitude=Double.parseDouble(sites.getString("sitelong" +
+							Integer.toString(i), ""));
+				
+					break;
+				}
 			}
-			if(i>=num_site)
+			if(i==num_site)
 			{
 			editor.putString("sitelat"+Integer.toString(num_site), Double.toString(Touch_latitude));
 			editor.putString("sitelong"+Integer.toString(num_site), Double.toString(Touch_longitude));
@@ -170,12 +358,18 @@ public class Activity01 extends MapActivity
 			PinOverlay pin=new PinOverlay(Touch_GeoPoint);
 			overlays.add(pin);
 			PinOverlays.add(pin);
+			
+			
 			}
-			else 
+			else
 			{
 				List<Overlay> overlays=mv.getOverlays();
 				for(int k=0;k<num_site;k++)
-				if(PinOverlays.get(k).GeoPinCoords.equals(Touch_GeoPoint)) 
+				if(
+						//PinOverlays.get(k).GeoPinCoords.equals(Touch_GeoPoint)
+						Touch_latitude==((double)PinOverlays.get(k).GeoPinCoords.getLatitudeE6())/1000000
+						&&Touch_longitude==((double)PinOverlays.get(k).GeoPinCoords.getLongitudeE6())/1000000
+						) 
 				{
 					overlays.remove(PinOverlays.get(k));
 					PinOverlays.remove(k);
@@ -194,8 +388,11 @@ public class Activity01 extends MapActivity
 				DisplayToast("删除。。。。。。");
 			}
 			editor.commit();
+			}
 			return true;
 		}
+		
+		
 		@Override
 		public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when)
 		{
@@ -207,9 +404,9 @@ public class Activity01 extends MapActivity
 			paint.setStrokeWidth(1);
 			paint.setARGB(255, 255, 0, 0);
 			paint.setStyle(Paint.Style.STROKE);
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.home);
-			canvas.drawBitmap(bmp, myScreenCoords.x, myScreenCoords.y, paint);
-			canvas.drawText("天府广场", myScreenCoords.x, myScreenCoords.y, paint);
+			//Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.home);
+			//canvas.drawBitmap(bmp, myScreenCoords.x, myScreenCoords.y, paint);
+			canvas.drawText("Here", myScreenCoords.x, myScreenCoords.y, paint);
 			return true;
 		}
 	}
